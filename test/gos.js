a133eb27b12910572c56d2ea0e92d329035de079
ironp <devascovitche@gmail.com>
2023-11-23 18:53:36 +0200

    add more tests

--- test/gos.js
+++ test/gos.js
@@ -1,11 +1,9 @@
 const Gos = artifacts.require('Gos');
 
 const {
-  BN,
-  constants,
-  expectEvent,
-  expectRevert,
-  time
+    expectEvent,
+    expectRevert,
+    time
 } = require('@openzeppelin/test-helpers');
 
 const amount = 10_000;
@@ -15,99 +13,155 @@ let deployer;
 
 contract('Gos', function (accounts) {
 
-  beforeEach(async function () {
-    gos = await Gos.new();
-    deployer = accounts[0];
-    holder = accounts[1];
-  });
-
-  it ('should be 200K tokens in total supply', async function () {
-    let totalTokens = await gos.totalSupply();
-    let deployerBalance = await gos.balanceOf(deployer);
-    let holderBalance = await gos.balanceOf(holder);
-
-    assert.equal(totalTokens, totalSupply, 'total supply should be 200K tokens');
-    assert.equal(deployerBalance, totalSupply, 'deployer should have 200K tokens');
-    assert.equal(holderBalance, 0, 'holder should have 0 tokens');
-  });
-
-  it ('should withdraw balance (mint) tokens for holder', async function () {
-    let tx = await gos.withdraw(holder, amount);
-    let checkTime = (await time.latest()).toNumber();
-
-    let totalTokens = await gos.totalSupply();
-    let deployerBalance = await gos.balanceOf(deployer);
-    let holderBalance = await gos.balanceOf(holder);
-
-    await expectEvent(tx, 'WithdrawBalance', {
-      user: holder,
-      amount: web3.utils.toBN(amount),
-      time: web3.utils.toBN(checkTime),
+    beforeEach(async function () {
+        gos = await Gos.new();
+        deployer = accounts[0];
+        holder = accounts[1];
     });
 
-    assert.equal(totalTokens, totalSupply + amount, 'total supply should be 200K plus 10K tokens');
-    assert.equal(deployerBalance, totalSupply, 'deployer should have 200K tokens');
-    assert.equal(holderBalance, amount, 'holder should have 10K tokens');
-  });
+    it('should be 200K tokens in total supply', async function () {
+        let totalTokens = await gos.totalSupply();
+        let deployerBalance = await gos.balanceOf(deployer);
+        let holderBalance = await gos.balanceOf(holder);
 
-  it ('should top up balance (burn) tokens for holder', async function () {
-    await gos.withdraw(holder, amount);
-
-    let totalTokens = await gos.totalSupply();
-
-    assert.equal(totalTokens, totalSupply + amount, 'total supply should be 200K plus 10K tokens');
-
-    let tx = await gos.topUp(holder, amount - 1_000);
-    let checkTime = (await time.latest()).toNumber();
-
-    await expectEvent(tx, 'TopUpBalance', {
-      user: holder,
-      amount: web3.utils.toBN(amount),
-      time: web3.utils.toBN(checkTime),
+        assert.equal(totalTokens, totalSupply, 'total supply should be 200K tokens');
+        assert.equal(deployerBalance, totalSupply, 'deployer should have 200K tokens');
+        assert.equal(holderBalance, 0, 'holder should have 0 tokens');
     });
 
-    totalTokens = await gos.totalSupply();
-    let deployerBalance = await gos.balanceOf(deployer);
-    let holderBalance = await gos.balanceOf(holder);
+    it('should top up balance (burn) tokens for holder', async function () {
+        await gos.transfer(holder, amount);
 
-    assert.equal(totalTokens, totalSupply + 1_000, 'total supply should be 200K plus 10K minus 9K tokens');
-    assert.equal(deployerBalance, totalSupply, 'deployer should have 200K tokens');
-    assert.equal(holderBalance, amount - 9_000, 'holder should have 1K tokens');
-  });
+        let totalTokens = await gos.totalSupply();
 
-  it ('should fee (mint) tokens for address (deployer)', async function () {
-    await gos.dappFee(deployer, amount);
+        assert.equal(totalTokens, totalSupply, 'total supply should be 200K tokens');
 
-    let totalTokens = await gos.totalSupply();
-    let deployerBalance = await gos.balanceOf(deployer);
-    let holderBalance = await gos.balanceOf(holder);
+        let tx = await gos.topUp(holder, amount - 1_000);
+        let checkTime = (await time.latest()).toNumber();
 
-    assert.equal(totalTokens, totalSupply + amount, 'total supply should be 200K plus 10K tokens');
-    assert.equal(deployerBalance, totalSupply + amount, 'deployer should have 200K plus 10K tokens');
-    assert.equal(holderBalance, 0, 'holder should have 0 tokens');
-  });
+        await expectEvent(tx, 'TopUpBalance', {
+            user: holder,
+            amount: web3.utils.toBN(amount - 1_000),
+            time: web3.utils.toBN(checkTime),
+        });
 
-  // it('staking should be successfully', async function () {
-  //   await sweet.approve(deployer, amount);
-  //   await sweet.transferFrom(deployer, holder, amount);
-  //   let tx = await sweet.staking(amount, hero1, {from: holder});
-  //   let checkTime = (await time.latest()).toNumber();
-  //
-  //   let holderBalance = await sweet.balanceOf(holder);
-  //   let deployerBalance = await sweet.balanceOf(deployer);
-  //
-  //   let hero1Votes = (await sweet.heroes.call(hero1)).toNumber();
-  //
-  //   assert.equal(holderBalance, 0, 'holder should have 0 tokens');
-  //   assert.equal(deployerBalance, 1e27 - amount, 'deployer should have 1e27 - 300 tokens');
-  //   assert.equal(hero1Votes, startVotesCount + amount, 'hero1 should have + 300 votes');
-  //
-  //   await expectEvent(tx, 'Staking', {
-  //     user: holder,
-  //     amount: web3.utils.toBN(amount),
-  //     hero: hero1,
-  //     startAt: web3.utils.toBN(checkTime),
-  //   });
-  // });
+        totalTokens = await gos.totalSupply();
+        let deployerBalance = await gos.balanceOf(deployer);
+        let holderBalance = await gos.balanceOf(holder);
+
+        assert.equal(totalTokens, totalSupply - 9_000, 'total supply should be 200K minus 9K tokens');
+        assert.equal(deployerBalance, totalSupply - amount, 'deployer should have 200K tokens');
+        assert.equal(holderBalance, amount - 9_000, 'holder should have 1K tokens');
+    });
+
+    it('should withdraw balance (mint) tokens for holder', async function () {
+        let tx = await gos.withdraw(holder, amount);
+        let checkTime = (await time.latest()).toNumber();
+
+        let totalTokens = await gos.totalSupply();
+        let deployerBalance = await gos.balanceOf(deployer);
+        let holderBalance = await gos.balanceOf(holder);
+
+        await expectEvent(tx, 'WithdrawBalance', {
+            user: holder,
+            amount: web3.utils.toBN(amount),
+            time: web3.utils.toBN(checkTime),
+        });
+
+        assert.equal(totalTokens, totalSupply + amount, 'total supply should be 200K plus 10K tokens');
+        assert.equal(deployerBalance, totalSupply, 'deployer should have 200K tokens');
+        assert.equal(holderBalance, amount, 'holder should have 10K tokens');
+    });
+
+    it('should withdraw dapp fee (mint) tokens for address (deployer)', async function () {
+        let tx = await gos.dappFee(deployer, amount);
+        let checkTime = (await time.latest()).toNumber();
+
+        await expectEvent(tx, 'DappFee', {
+            to: deployer,
+            amount: web3.utils.toBN(amount),
+            time: web3.utils.toBN(checkTime),
+        });
+
+        let totalTokens = await gos.totalSupply();
+        let deployerBalance = await gos.balanceOf(deployer);
+        let holderBalance = await gos.balanceOf(holder);
+
+        assert.equal(totalTokens, totalSupply + amount, 'total supply should be 200K plus 10K tokens');
+        assert.equal(deployerBalance, totalSupply + amount, 'deployer should have 200K plus 10K tokens');
+        assert.equal(holderBalance, 0, 'holder should have 0 tokens');
+    });
+
+    it('revert top up tokens (burn) tokens if not owner', async function () {
+        await gos.transfer(holder, amount);
+
+        let totalTokens = await gos.totalSupply();
+
+        assert.equal(totalTokens, totalSupply, 'total supply should be 200K tokens');
+
+        await expectRevert(
+            gos.topUp(holder, amount - 1_000, {from: holder}),
+            'Ownable: caller is not the owner');
+
+        let totalTokensAfterRevert = await gos.totalSupply();
+
+        assert.equal(totalTokens.toNumber(),
+            totalTokensAfterRevert.toNumber(),
+            'total supply should be 200K tokens');
+    });
+
+    it('revert withdraw tokens (mint) tokens if not owner', async function () {
+        let totalTokens = await gos.totalSupply();
+
+        await expectRevert(
+            gos.withdraw(holder, amount, {from: holder}),
+            'Ownable: caller is not the owner');
+
+        let totalTokensAfterRevert = await gos.totalSupply();
+
+        assert.equal(totalTokens.toNumber(),
+            totalTokensAfterRevert.toNumber(),
+            'total supply should be 200K tokens');
+    });
+
+    it('revert dapp fee (mint) tokens if not owner', async function () {
+        let totalTokens = await gos.totalSupply();
+
+        await expectRevert(
+            gos.dappFee(deployer, amount, {from: holder}),
+            'Ownable: caller is not the owner');
+
+        let totalTokensAfterRevert = await gos.totalSupply();
+
+        assert.equal(totalTokens.toNumber(),
+            totalTokensAfterRevert.toNumber(),
+            'total supply should be 200K tokens');
+    });
+
+    it('transfer is not ownable', async function () {
+        let totalTokens = await gos.totalSupply();
+
+        let tx = await gos.transfer(holder, amount);
+
+        await expectEvent(tx, 'Transfer', {
+            from: deployer,
+            to: holder,
+            value: web3.utils.toBN(amount),
+        });
+
+        let txFromHolder = await gos.transfer(deployer, amount, {from: holder});
+
+        await expectEvent(txFromHolder, 'Transfer', {
+            from: holder,
+            to: deployer,
+            value: web3.utils.toBN(amount),
+        });
+
+        let totalTokensAfterTxs = await gos.totalSupply();
+
+        assert.equal(totalTokens.toNumber(),
+            totalTokensAfterTxs.toNumber(),
+            'total supply should be 200K tokens');
+    });
 
 });
